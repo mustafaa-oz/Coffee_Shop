c4bbf6e4c759605113bd1e4a8669ecbc9539c006
Mustafa ÖZ <mustafaa.oz@outlook.com>
2025-05-12 22:54:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 import streamlit as st
 
+st.set_page_config(
+    page_title="Miuul Coffee Shop",
+    page_icon="☕",
+    layout="wide",
+)
 
-
-home_page = st.Page(page="pages/HomePage.py", title="Ana Sayfa")
-lokasyon = st.Page(page="pages/Lokasyon.py", title="Lokasyon (Admin)")
-siparis = st.Page(page="pages/Siparis.py", title="Sipariş Ekranı")
-kar_hesapla = st.Page(page="pages/Kar_Hesapla.py", title="Günlük Kâr Hesapla")
-Model_degerlendirmesi = st.Page(page="pages/Model_degerlendirmesi.py", title="Model Değerlendirmesi")
-
-pg = st.navigation([home_page, lokasyon, kar_hesapla, siparis, Model_degerlendirmesi])
-
-pg.run()
+st.title("☕ Miuul Coffee Shop")
+st.write("Sol taraftan istediğin sayfayı seç.")
 
